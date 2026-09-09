0bc8810f0efa8a647fa05569f585747f33102707
inoueyuji <sikogaku2593@gmail.com>
2026-09-09 23:46:42 +0900

    learn-hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,5 +5,14 @@ const app = new Hono();
 app.get("/", (c) => {
   return c.json({ message: "Hello Hono!" });
 });
+app.get("/users/:id", (c) => {
+  const id = c.req.param("id");
+  return c.json({ id, name: `User ${id}` });
+});
+app.get("/search", (c) => {
+  const q = c.req.query("q");
+  const qq = c.req.query();
+  return c.json({ q, qq });
+});
 
 export default app;
