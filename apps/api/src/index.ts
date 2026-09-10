a1c0c9a4c775144a681422e697200fac6563b022
inoueyuji <sikogaku2593@gmail.com>
2026-09-10 21:07:36 +0900

    learn-hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -15,4 +15,15 @@ app.get("/search", (c) => {
   return c.json({ q, qq });
 });
 
+app.post("/post", async (c) => {
+  const body = await c.req.json();
+  const id = body.id;
+  const name = body.name;
+  return c.json({
+    body,
+    message: "post successful",
+    response: { id, name },
+  });
+});
+
 export default app;
