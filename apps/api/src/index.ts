350abfd35732a343ec98694f88a7b8a6f663f32b
inoueyuji <sikogaku2593@gmail.com>
2026-09-14 22:23:36 +0900

    learn-hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,10 +3,21 @@ import users from "./routes/users";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello Hono!" });
-});
-
 app.route("/users", users);
 
+app.use("*", async (c, next) => {
+  console.log("middleware A start");
+  await next();
+  console.log("middleware A end");
+});
+
+app.use("*", async (c, next) => {
+  console.log("middleware B start");
+  await next();
+  console.log("middleware B end");
+});
+app.get("/", (c) => {
+  console.log("middleware");
+  return c.json({ message: "Hello Hono!" });
+});
 export default app;
