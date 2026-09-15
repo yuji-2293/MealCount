69a2f31b565272b040d43979bcd36fc682c2db16
inoueyuji <sikogaku2593@gmail.com>
2026-09-15 21:46:48 +0900

    learn-hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,19 +3,32 @@ import users from "./routes/users";
 
 const app = new Hono();
 
-app.route("/users", users);
-
+// Middleware A
+// useはグローバルに全てのルートに対してMiddlewareを登録する
 app.use("*", async (c, next) => {
   console.log("middleware A start");
   await next();
   console.log("middleware A end");
 });
-
+// Middleware B
 app.use("*", async (c, next) => {
   console.log("middleware B start");
   await next();
   console.log("middleware B end");
 });
+
+app.use("*", async (c, next) => {
+  const isAuthenticated = false;
+  if (!isAuthenticated) {
+    c.status(401);
+
+    return c.json({ message: "Unauthorized", error: "Not authenticated" });
+  }
+  await next();
+});
+
+app.route("/users", users);
+
 app.get("/", (c) => {
   console.log("middleware");
   return c.json({ message: "Hello Hono!" });
