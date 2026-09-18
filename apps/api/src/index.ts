404e37453dd4f2a5a06a5695556d62f7a50aa921
inoueyuji <sikogaku2593@gmail.com>
2026-09-18 17:19:51 +0900

    learn-hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -9,34 +9,4 @@ app.get("/", (c) => {
 });
 app.route("/users", users);
 
-// zod schemas
-// zodの挙動検証
-const stringSchema = z.string();
-const numberSchema = z.number();
-const booleanSchema = z.boolean();
-const dateSchema = z.date();
-
-const objectSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-});
-
-app.post("/validation", zValidator("json", objectSchema), (c) => {
-  const data = c.req.valid("json");
-  console.log(data);
-  return c.json(data);
-});
-
-// curlでPOSTリクエストを送る例
-
-// curl -i -X POST http://localhost:8787/validation \
-//   -H "Content-Type: application/json" \
-//   -d '{"id":1,"name":"yuji"}'
-
-// オプション説明
-// -i  → Response Headerも表示
-// -X  → HTTP Method
-// -H  → Request Header
-// -d  → Request Body
-
 export default app;
